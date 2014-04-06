313be7d32b68005ffd2856bb428fa28011236120
gabe <balcides@gmail.com>
2014-04-06 02:51:29 -0700

    fixing scripts folder issue
    fixing scripts folder issue

---
+++ Assets/spScripts/Game.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+/// Game.cs
+/// 
+/// This is designed to run the game (runtime scripts)
+/// 
+/// Author: Gabe Betancourt
+/// 
+/// </summary>
+
+
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
