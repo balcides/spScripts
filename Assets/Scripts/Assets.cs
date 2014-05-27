9aa2fc0eab874a3c91c9a3dc2dd631454ac1d7f0
gabe <balcides@hotmail.com>
2014-05-26 17:43:02 -0700

    PC - simple lib file test

--- Assets/Scripts/Assets.cs
+++ Assets/Scripts/Assets.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Assets.cs
+/// 
+/// This script was designed to load and keep track of assets for the game
+/// </summary>
+
 public class Assets : MonoBehaviour {
 
 	// Use this for initialization
